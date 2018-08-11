9176792085ab9b21411b7759c58d04f4176769f6
Howard Yeh <hayeah@gmail.com>
2018-08-11 13:59:13 +0200

    fix gasPrice satoshi conversion

--- src/WalletRPCProvider.ts
+++ src/WalletRPCProvider.ts
@@ -5,23 +5,29 @@ import { Wallet } from "./Wallet"
 
 export class WalletRPCProvider implements IProvider {
 
-  constructor(public wallet: Wallet) {}
+  constructor(public wallet: Wallet) { }
 
   public rawCall(
     method: string,
     params: any[] = [],
     opts: any = {}): Promise<Insight.IContractCall | Insight.ISendRawTxResult> {
-      const [contractAddress, encodedData, amount = 0, gasLimit = 200000, gasPrice = 40] = params
-      opts = Object.assign({ amount, gasLimit, gasPrice }, opts)
+    const [contractAddress, encodedData, amount = 0, gasLimit = 200000, gasPrice = 0.0000004] = params
 
-      switch (method.toLowerCase()) {
-        case "sendtocontract":
-          return this.wallet.contractSend(contractAddress, encodedData, opts)
-        case "callcontract":
-          return this.wallet.contractCall(contractAddress, encodedData, opts)
-        default:
-          throw new Error("Unknow method call")
-      }
+    // The underlying qtumjs-wallet API expects gasPrice to be specified in sat
+    const gasPriceInSatoshi = Math.floor(gasPrice * 1e8)
+    opts = {
+      ...opts,
+      gasPrice: gasPriceInSatoshi,
+    }
+
+    switch (method.toLowerCase()) {
+      case "sendtocontract":
+        return this.wallet.contractSend(contractAddress, encodedData, opts)
+      case "callcontract":
+        return this.wallet.contractCall(contractAddress, encodedData, opts)
+      default:
+        throw new Error("Unknow method call")
+    }
   }
 
   public cancelTokenSource(): CancelTokenSource {
